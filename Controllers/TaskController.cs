e1c0e62eb02309a5216996f63bc56a970678169d
Oleksandr Korenivskyi <sana22_koo@student.ztu.edu.ua>
2023-04-09 21:49:43 +0300

    Added view for task list, added actions for working with tasks in TaskController (only mocked data)

--- Controllers/TaskController.cs
+++ Controllers/TaskController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 using ToDoList.Models;
 
 namespace ToDoList.Controllers
@@ -8,7 +7,46 @@ namespace ToDoList.Controllers
     {
         public IActionResult List()
         {
-            return View();
+            //getting info from database
+            var model = GetListPageModel();
+            return View(model);
+        }
+
+        public IActionResult Create(string newTaskDescription, DateTime? newTaskDueDate, Guid newTaskCategoryId)
+        {
+            //saving into database
+            return RedirectToAction("List");
+        }
+
+        public IActionResult Delete(Guid taskId)
+        {
+            //deliting from database
+            return RedirectToAction("List");
+        }
+
+        public IActionResult ToggleComplition(Guid taskId)
+        {
+            //changing Completed value in database
+            return RedirectToAction("List");
+        }
+
+        private ListPageModel GetListPageModel()
+        {
+            var listPageModel = new ListPageModel();
+            listPageModel.Categories = new List<CategoryModel>
+            {
+                new CategoryModel(){Id = Guid.NewGuid(), Name = "Housework",},
+                new CategoryModel(){Id = Guid.NewGuid(), Name = "Sport",},
+                new CategoryModel(){Id = Guid.NewGuid(), Name = "Job",}
+            };
+
+            listPageModel.Tasks = new List<TaskModel>
+            {
+                new TaskModel() { Description = "Make a dinner", DueDate = new DateTime(), CategoryName = "Housework", Id = Guid.NewGuid() },
+                new TaskModel() { Description = "Make a coffee", DueDate = new DateTime(), CategoryName = "Housework", Id = Guid.NewGuid() },
+                new TaskModel() { Description = "Make a tea", DueDate = new DateTime(), CategoryName = "Housework", Id = Guid.NewGuid() },
+            };
+            return listPageModel;
         }
     }
 }
--- Models/ListPageModel.cs
+++ Models/ListPageModel.cs
@@ -1,8 +1,17 @@
-﻿namespace ToDoList.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Models
 {
     public class ListPageModel
     {
-        public CreateTaskModel CreateTaskModel { get; set; }
+        public string NewTaskDescription { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime? NewTaskDueDate { get; set; }
+
+        public List<CategoryModel> Categories { get; set; }
+
+        public Guid NewTaskCategoryId { get; set; }
 
         public List<TaskModel> Tasks { get; set; }
     }
