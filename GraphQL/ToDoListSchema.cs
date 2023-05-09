51bca59de4dc859ec9a386087e88d9e19084a74a
Oleksandr Korenivskyi <sana22_koo@student.ztu.edu.ua>
2023-05-09 16:28:36 +0300

    Cleanup

--- GraphQL/ToDoListSchema.cs
+++ GraphQL/ToDoListSchema.cs
@@ -4,7 +4,7 @@ namespace ToDoList.GraphQL
 {
     public class ToDoListSchema : Schema
     {
-        public ToDoListSchema(IServiceProvider provider, TodoListQueryType todoListQuery, ToDoListMutationType todoListMutation)
+        public ToDoListSchema(IServiceProvider provider, TodoListQueryType todoListQuery, ToDoListMutationType todoListMutation) 
        : base(provider)
         {
             Query = todoListQuery;
